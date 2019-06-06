88c601f61fcf835d1717f55262475ed8f68b6741
tim <cd0932507137@yahoo.com.tw>
2019-06-06 17:13:15 +0800

    添加餅圖與儀錶盤

--- src/main.js
+++ src/main.js
@@ -9,11 +9,17 @@ import { i18n } from './plugins/i18'
 import VCharts from 'v-charts'
 // 折线图
 import VeLine from 'v-charts/lib/line.common'
+import VePie from 'v-charts/lib/pie.common'
+import VeGauge from 'v-charts/lib/gauge.common'
 
 Vue.use(VueAxios, axios, VCharts)
 Vue.config.productionTip = false
 // 折线图
 Vue.component(VeLine.name, VeLine)
+// 饼图
+Vue.component(VePie.name, VePie)
+// 仪表盘
+Vue.component(VeGauge.name, VeGauge)
 
 /* eslint-disable no-new */
 new Vue({
