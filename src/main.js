e73cfb7b4bd95c5e56ac115630dd309fbd7748d6
tim <cd0932507137@yahoo.com.tw>
2019-06-26 16:07:24 +0800

    添加圖表切換功能

--- src/main.js
+++ src/main.js
@@ -7,19 +7,24 @@ import App from './App'
 import router from './router'
 import { i18n } from './plugins/i18'
 import VCharts from 'v-charts'
-// 折线图
+// 圖表
 import VeLine from 'v-charts/lib/line.common'
 import VePie from 'v-charts/lib/pie.common'
 import VeGauge from 'v-charts/lib/gauge.common'
+import VeHistogram from 'v-charts/lib/histogram.common'
+import VeChart from 'v-charts/lib/chart.common'
 
 Vue.use(VueAxios, axios, VCharts)
 Vue.config.productionTip = false
-// 折线图
+// 折線圖
 Vue.component(VeLine.name, VeLine)
-// 饼图
+// 餅圖
 Vue.component(VePie.name, VePie)
-// 仪表盘
+// 儀表盤
 Vue.component(VeGauge.name, VeGauge)
+// 柱狀圖
+Vue.component(VeHistogram.name, VeHistogram)
+Vue.component(VeChart.name, VeChart)
 
 /* eslint-disable no-new */
 new Vue({
